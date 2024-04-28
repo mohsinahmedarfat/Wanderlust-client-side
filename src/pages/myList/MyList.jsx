64199bc0178eb42380f1c0199e7150208147d08f
rfaraf <mohsinarfat53@gamil.com>
2024-04-28 11:59:23 +0600

    implemented myList page with certain user's tourist spot in a table

--- src/pages/myList/MyList.jsx
+++ src/pages/myList/MyList.jsx
@@ -1,20 +1,53 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const MyList = () => {
   const { user } = useContext(AuthContext);
+  const [spots, setSpots] = useState([]);
+  console.log(spots);
 
   useEffect(() => {
     fetch(`http://localhost:5000/myList/${user.email}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setSpots(data);
       });
   }, [user]);
 
   return (
-    <div>
-      <h1>my list</h1>
+    <div className="my-10">
+      <h1 className="text-3xl text-center mb-5 font-bold">My Tourist Spots</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Tourist Spot Name</th>
+              <th>Country</th>
+              <th>Average Cost</th>
+              <th>Seasonality</th>
+            </tr>
+          </thead>
+          <tbody>
+            {spots.map((spot, idx) => (
+              <tr key={spot._id} className="hover">
+                <th>{idx + 1}</th>
+                <td>{spot.tourists_spot_name}</td>
+                <td>{spot.country_name}</td>
+                <td>{spot.average_cost}</td>
+                <td>{spot.seasonality}</td>
+                <td>
+                  <button className="btn">Update</button>
+                </td>
+                <td>
+                  <button className="btn">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
